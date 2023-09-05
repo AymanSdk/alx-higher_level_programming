1a33526b716825bfd3ff39ffae387e9bab116de0
AymanSdk <ayman.sdk123@outlook.com>
2023-09-05 15:16:57 +0100

    first else if elif??

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(f"{number} is possitive")
+    print(f"{number} is positive")
 elif number == 0:
     print(f"{number} is zero")
 else:
