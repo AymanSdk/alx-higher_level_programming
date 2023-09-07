24a7ce93dcdeebc0c75410b19cf7b6cd059e2808
AymanSdk <ayman.sdk123@outlook.com>
2023-09-07 16:58:35 +0100

    indentaaaaa

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import sys
 
 count = len(sys.argv) - 1
@@ -10,4 +9,4 @@ elif count == 1:
 else:
     print("{} arguments:".format(count))
 for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
