981a886f7973a7bfa05cf5785dfbf0fc2990bb99
AymanSdk <ayman.sdk123@outlook.com>
2023-09-07 20:34:34 +0100

    Fixing things

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+if __name__ == "__main__":
+    import sys
+
+    count = len(sys.argv) - 1
+    su = 0
+    if count == 0:
+        print("0")
+    else:
+        for i in range(1, count + 1):
+            su += int(sys.argv[i])
+        print("{}".format(su))
