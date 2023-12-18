92dbbd1ceff496706d2807ae3fd066080ff5690a
AymanSdk <ayman.sdk123@oulook.com>
2023-12-18 01:06:06 +0100

    rreverting

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,19 +1,33 @@
 #!/usr/bin/python3
-"""List all states"""
-from sys import argv
-from model_state import Base, State
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+"""
+    A script that lists all State objects from hbtn_0e_6_usa that conatin
+    the letter a from teh database.
+    Username, password and dbname wil be passed as arguments to the script.
+"""
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'
-        .format(argv[1], argv[2],
-                argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
+    # create a session
     session = Session()
-    for state in session.query(State).\
-            filter(State.name.like('%a%')).order_by(State.id).all():
+
+    # extract first state
+    states = session.query(State).filter(State.name.ilike('%a%')) \
+                    .order_by(State.id).all()
+
+    # print states
+    for state in states:
         print("{}: {}".format(state.id, state.name))
+
     session.close()
