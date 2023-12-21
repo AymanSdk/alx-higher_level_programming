73bc7996bd975a4e2e3da85df1f37fcf8afb1ccc
AymanSdk <ayman.sdk123@oulook.com>
2023-12-21 19:28:50 +0100

    PycodeStyle Fixed

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,6 +4,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from relationship_state import Base
 
+
 class City(Base):
     """Class representing the states table"""
     __tablename__ = 'cities'
